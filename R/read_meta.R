03e3ad7dfe10c21b593fcdaf113d6ae697dda12b
cmagister <paul.mcelhany@noaa.gov>
2021-02-02 16:28:42 -0800

    Added some doumentation to the package

--- R/read_meta.R
+++ R/read_meta.R
@@ -1,6 +1,20 @@
-readMeta <- function(fileName){
+#' Read csv file with metadata
+#'
+#' @param file path and name of the .csv data file to read
+#'
+#' @return List with three data frames
+#'    \describe{
+#'     \item{dGeneralMeta}{General metadata with attribute and value}
+#'     \item{dVariableMeta}{Table of metadata for each dataset variable}
+#'     \item{dData}{The dataset}
+#'    }
+#' @export
+#'
+#' @examples
+#'
+read_meta <- function(file){
   #read the first row of the data file
-  inFile <- read.csv(fileName, stringsAsFactors=FALSE, header = FALSE)
+  inFile <- read.csv(file, stringsAsFactors=FALSE, header = FALSE)
   #test whether the file has NWFSC OA format metadata,
   #which starts with "NWFSC_OA_format_file" in the first cell
   if(inFile[1,1] == "NWFSC_OA_format_file"){
@@ -9,12 +23,12 @@ readMeta <- function(fileName){
     endVariableMetaMarkerRow <- match("###", inFile[,1])
     #read the general metadata info and clean it up so first column
     # is the attribute name and the second column is the value of the attribute
-    dGeneralMeta <- read.csv(fileName, stringsAsFactors=FALSE,
+    dGeneralMeta <- read.csv(file, stringsAsFactors=FALSE,
                              header = FALSE, skip = 1, nrows = endGeneralMetaMakerRow - 2)
     dGeneralMeta <- dGeneralMeta[,1:2]
     names(dGeneralMeta) <- c("Attribute", "Value")
     #Read in the variable metadata table and clean it up
-    dVariableMeta <- read.csv(fileName, stringsAsFactors=FALSE,
+    dVariableMeta <- read.csv(file, stringsAsFactors=FALSE,
                               header = FALSE, skip = endGeneralMetaMakerRow,
                               nrows = endVariableMetaMarkerRow - endGeneralMetaMakerRow - 1)
     attributeNames <- dVariableMeta[1,]
@@ -22,14 +36,14 @@ readMeta <- function(fileName){
     dVariableMeta <- dVariableMeta[2:length(dVariableMeta[,1]), 1:length(attributeNames)]
     names(dVariableMeta) <- attributeNames
     #Read in the meta data file
-    dData <- read.csv(fileName, stringsAsFactors=FALSE,
+    dData <- read.csv(file, stringsAsFactors=FALSE,
                       header = TRUE, skip = endVariableMetaMarkerRow)
     #need to add some code here to remove columns in dData if
     # variable name starts with "x" and all values in the column are NA
     # this situation happens if the number of variables in the data set are < length(attributeNames)
   } else{
     #name of potential seperate metadata file
-    metaFileName <- word(fileName, sep = ".csv")
+    metaFileName <- word(file, sep = ".csv")
     metaFileName <- paste(metaFileName, "_meta.csv", sep = "")
     if(file.exists(metaFileName)){
       dMeta <- read.csv(metaFileName, stringsAsFactors = FALSE, header = FALSE)
@@ -45,7 +59,7 @@ readMeta <- function(fileName){
     else{
       dGeneralMeta <- NULL
       dVariableMeta <- NULL
-      dData <- read.csv(fileName, stringsAsFactors=FALSE, header = TRUE)
+      dData <- read.csv(file, stringsAsFactors=FALSE, header = TRUE)
     }
   }
 
---
+++ README.md
@@ -0,0 +1,10 @@
+
+# metacsv
+
+The goal of metacsv is to provide functions for reading and writing
+metadata for .csv data files that is either embedded at the top of the
+file or in an associated csv file.
+
+## Installation
+
+## Example
